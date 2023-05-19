33f8db157bc526587d01588ffe3e61e38f4c5220
Christian Göttsche <cgzones@googlemail.com>
2023-05-19 21:10:16 +0200

    build-helper: improve output

--- build-helper.sh
+++ build-helper.sh
@@ -23,20 +23,29 @@ function log {
 # Install extra dependencies that were provided for the build (if any)
 #   Note: dpkg can fail due to dependencies, ignore errors, and use
 #   apt-get to install those afterwards
-[[ -d /dependencies ]] && dpkg -i /dependencies/*.deb || apt-get -f install -y --no-install-recommends
+if [ -d /dependencies ]; then
+    log "Installing dependencies"
+    dpkg -i /dependencies/*.deb
+    apt-get -f install -y --no-install-recommends
+fi
 
 # Make read-write copy of source code
+log "Copying source directory"
 mkdir -p /build
 cp -a /source-ro /build/source
 cd /build/source
 
 # Install build dependencies
+log "Installing build dependencies"
 mk-build-deps -ir -t "apt-get -o Debug::pkgProblemResolver=yes -y --no-install-recommends"
 
 # Build packages
+log "Building package"
 debuild -b -uc -us
 
 # Copy packages to output dir with user's permissions
-chown -R $USER:$GROUP /build
+chown -R "$USER:$GROUP" /build
 cp -a /build/*.deb /build/*.buildinfo /build/*.changes /output/
-ls -l /output
+ls -l -A --color=auto -h /output
+
+log "Finished"
